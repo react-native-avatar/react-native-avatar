7ec0dc8d15a358ee9e89b566b0d2c8c9c00d06ed
minpppal <minpppal@gmail.com>
2025-07-01 17:04:28 +0900

    기초 컴포넌트, flexbox

--- app/(tabs)/index.tsx
+++ app/(tabs)/index.tsx
@@ -1,19 +1,39 @@
-import { StyleSheet, Text, View } from "react-native";
+import { SafeAreaView, StyleSheet, Text, View } from "react-native";
 
 export default function HomeScreen() {
   return (
-    <View style={styles.container}>
-      <Text style={styles.text}>텍스트</Text>
-    </View>
+    <SafeAreaView>
+      <View style={styles.parent}>
+        <View style={styles.container}>
+          <Text style={styles.text}>텍스트</Text>
+        </View>
+        <View style={styles.container2}>
+          <Text style={styles.text}>텍스트</Text>
+          <Text style={styles.text}>텍스트</Text>
+          <Text style={styles.text}>텍스트</Text>
+        </View>
+      </View>
+    </SafeAreaView>
   );
 }
 
 const styles = StyleSheet.create({
+  parent: {
+    flexDirection: "row",
+    justifyContent: "center",
+    alignItems: "center",
+  },
   container: {
     backgroundColor: "yellow",
   },
+  container2: {
+    backgroundColor: "blue",
+  },
   text: {
     color: "red",
+    fontSize: 30,
+  },
+  input: {
     fontSize: 30,
   },
 });
