0dbc4c5f03b9127ec759c7fcf25bf23de7685ed4
minpppal <minpppal@gmail.com>
2025-07-01 21:57:09 +0900

    feat: 탭바 css 구현, expo 아이콘 사용

--- app/(tabs)/_layout.tsx
+++ app/(tabs)/_layout.tsx
@@ -1,3 +1,5 @@
+import { colors } from "@/constants";
+import Ionicons from "@expo/vector-icons/Ionicons";
 import { Tabs } from "expo-router";
 import React from "react";
 
@@ -5,20 +7,47 @@ export default function TabLayout() {
   return (
     <Tabs
       screenOptions={{
-        tabBarActiveTintColor: "black",
+        tabBarActiveTintColor: colors.ORANGE_600,
         headerShown: false,
       }}
     >
       <Tabs.Screen
         name="index"
         options={{
-          title: "Home",
+          title: "홈",
+          tabBarIcon: ({ color, focused }) => (
+            <Ionicons
+              name={focused ? "home-sharp" : "home-outline"}
+              size={25}
+              color={color}
+            />
+          ),
         }}
       />
       <Tabs.Screen
-        name="explore"
+        name="my"
         options={{
-          title: "Explore",
+          title: "내 프로필",
+          tabBarIcon: ({ color, focused }) => (
+            <Ionicons
+              name={focused ? "person-circle" : "person-circle-outline"}
+              size={25}
+              color={color}
+            />
+          ),
+        }}
+      />
+      <Tabs.Screen
+        name="setting"
+        options={{
+          title: "설정",
+          tabBarIcon: ({ color, focused }) => (
+            <Ionicons
+              name={focused ? "settings" : "settings-outline"}
+              size={25}
+              color={color}
+            />
+          ),
         }}
       />
     </Tabs>
--- app/(tabs)/index.tsx
+++ app/(tabs)/index.tsx
@@ -1,39 +1,12 @@
-import { SafeAreaView, StyleSheet, Text, View } from "react-native";
+import { Pressable, Text } from "react-native";
+import { SafeAreaView } from "react-native-safe-area-context";
 
 export default function HomeScreen() {
   return (
     <SafeAreaView>
-      <View style={styles.parent}>
-        <View style={styles.container}>
-          <Text style={styles.text}>텍스트</Text>
-        </View>
-        <View style={styles.container2}>
-          <Text style={styles.text}>텍스트</Text>
-          <Text style={styles.text}>텍스트</Text>
-          <Text style={styles.text}>텍스트</Text>
-        </View>
-      </View>
+      <Pressable>
+        <Text>홈스크린</Text>
+      </Pressable>
     </SafeAreaView>
   );
 }
-
-const styles = StyleSheet.create({
-  parent: {
-    flexDirection: "row",
-    justifyContent: "center",
-    alignItems: "center",
-  },
-  container: {
-    backgroundColor: "yellow",
-  },
-  container2: {
-    backgroundColor: "blue",
-  },
-  text: {
-    color: "red",
-    fontSize: 30,
-  },
-  input: {
-    fontSize: 30,
-  },
-});
