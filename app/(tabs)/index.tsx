3d0b6018298c1523f3c011a22e8018beea056540
minpppal <minpppal@gmail.com>
2025-07-10 16:15:17 +0900

    feat: 인풋 컴포넌트 구현

--- app/(tabs)/index.tsx
+++ app/(tabs)/index.tsx
@@ -1,12 +1,13 @@
-import { Pressable, Text } from "react-native";
+import CustomButton from "@/components/CustomButton";
+import { router } from "expo-router";
+import { Text } from "react-native";
 import { SafeAreaView } from "react-native-safe-area-context";
 
 export default function HomeScreen() {
   return (
     <SafeAreaView>
-      <Pressable>
-        <Text>홈스크린</Text>
-      </Pressable>
+      <Text>홈스크린</Text>
+      <CustomButton label="버튼" onPress={() => router.push("/auth")} />
     </SafeAreaView>
   );
 }
--- app/_layout.tsx
+++ app/_layout.tsx
@@ -14,6 +14,7 @@ export default function RootLayout() {
   return (
     <Stack>
       <Stack.Screen name="(tabs)" options={{ headerShown: false }} />
+      <Stack.Screen name="auth" options={{ headerShown: false }} />
       <Stack.Screen name="+not-found" />
     </Stack>
   );
