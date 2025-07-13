bb001ab59deef0f68e9527249993e355de7e1f34
minpppal <minpppal@gmail.com>
2025-07-13 17:56:37 +0900

    feat: rules,validate 사용해서 유효성검사 구현

--- components/EmailInput.tsx
+++ components/EmailInput.tsx
@@ -9,12 +9,24 @@ function EmailInput() {
     <Controller
       name="email"
       control={control}
-      render={({ field: { onChange, value } }) => (
+      //유효성 검사
+      rules={{
+        validate: (data: string) => {
+          if (data.length === 0) {
+            return "이메일을 입력해주세요.";
+          }
+          if (!/^[^\s@]+@[^\s@]+\.[^\s@]+$/.test(data)) {
+            return "올바른 이메일 형식이 아닙니다.";
+          }
+        },
+      }}
+      render={({ field: { onChange, value }, fieldState: { error } }) => (
         <InputField
           label="이메일"
           placeholder="이메일을 입력해주세요."
           value={value}
           onChangeText={onChange}
+          error={error?.message}
         />
       )}
     />
